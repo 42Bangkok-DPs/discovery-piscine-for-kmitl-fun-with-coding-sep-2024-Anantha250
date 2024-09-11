ea4d99d08f32a75a4ba679e3163c7b3fd9d9fe39
Anantha250 <129850658+Anantha250@users.noreply.github.com>
2024-09-11 13:41:12 +0700

    Update count_files.sh

--- cell00/ex03/count_files.sh
+++ cell00/ex03/count_files.sh
@@ -3,5 +3,5 @@ file_count=$(find . -maxdepth 1 -type f | wc -l)
 
 dir_count=$(find . -maxdepth 1 -type d | wc -l)
 
-echo "Number of regular files: $file_count"
-echo "Number of directories: $dir_count"
+echo $file_count
+echo $dir_count
